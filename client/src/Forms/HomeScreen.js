e71391ab4aa29681c1db0b4bc7230bec1aca7966
ryanvu2 <ryanvuemail@gmail.com>
2023-11-30 17:37:59 -0800

    Addition: Broswer Title, Favico Update: HomeScreen CSS

--- client/src/Forms/HomeScreen.js
+++ client/src/Forms/HomeScreen.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useNavigate} from "react-router-dom"
 import GroupCard from "../GroupCard";
 import ProfileCard from "../ProfileCard";
+import React from "react";
 
 function HomeScreen(){
     const [userFirstName, setUserFirstName] = useState ("INSERT FIRST NAME")
@@ -17,11 +18,15 @@ function HomeScreen(){
     const [groupPic, setGroupPic] = useState ('https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/640px-Image_not_available.png')
     const [numMembers, setNumMembers] = useState (999)
 
+    // const navigate = useNavigate();
+
     return(
 <div>
     {/* header outside bc margins */}
-    <div>
-        <h1 class = "homeHeader">Alumni Avenue</h1>
+    <div  class = "homeHeader">
+        <img class = "homeLogo" src = '/AlumAveLogo.png' alt = "Alumni Avenue Logo"/>
+        <h1>Alumni Avenue</h1>
+        <button>Profile</button>
     </div>
     {/* container of everything besides header */}
     <div class = "homeContainer">
@@ -30,7 +35,12 @@ function HomeScreen(){
        </div>
         <div class = "homeProfile">
             <h2>My Profile</h2>
-            <button class = "homeBtn">Edit Profile</button>
+            <button 
+            class = "homeBtn" 
+            // onclick = {() => navigate('/signup')}
+            >
+                Edit Profile
+            </button>
         </div>
         <ProfileCard uName = {userName} uSchool = {userSchool} uJob = {userJob} uClass = {userClass} uPic = {profilePic}/>
     
