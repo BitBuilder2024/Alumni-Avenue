33f6d6b7d9363a27927d3b8282bb54fef86f6d0c
tomohto <toh2737@gmail.com>
2023-12-05 12:00:34 -0800

    Quick edits to App.js after completing manual merge

--- Fronend/src/App.js
+++ Fronend/src/App.js
@@ -6,14 +6,15 @@ import Signup from './Signup/Signup';
 import Update from './Update/Update'; // Import the Update component
 import './App.css';
 
+
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/forgotPassword" element={<ForgotPassword />} />
-        <Route path="/Components/Signup" element={<Signup />} />
-        <Route path="/update" element={<Update />} />
+        <Route path="/Components/Signup" element={<Signup />} /> 
+        <Route path="/update" element={<Update />} /> 
       </Routes>
     </Router>
   );
