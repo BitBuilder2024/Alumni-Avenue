c4f770fd98f26ab6de22f1ebb36cb6e4b8b923c3
racheymonkey <131830335+racheymonkey@users.noreply.github.com>
2023-12-05 17:50:23 -0800

    Update SearchedGroups.js

--- client/src/Components/SearchedGroups.js
+++ client/src/Components/SearchedGroups.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import '../JoinGroup/JoinGroup.css';
 
 function SearchedGroup(props) {
     // Track if group is clicked
@@ -12,23 +13,27 @@ function SearchedGroup(props) {
     return(
         <div>
         <div className = "searched-group" onClick={handleClick}>
-            <div className="left-searched-group">
-                <p class = "joinGroupName">{props.gName}</p>
-                <img class = "joinGroupPic" src = {props.gPic} alt = "Group Picture" />
-            </div>
-            <div className="right-searched-group">
-                <p>{props.nMem} Members</p>
+            <div className="components-searched-group">
+                <div className="top-searched-group">
+                    <div className="left-searched-group">
+                        <img class = "joinGroupPic" src = {props.gPic} alt = "Group Picture" />
+                        <p class = "joinGroupName">{props.gName}</p>
+                    </div>
+                    <div className="right-searched-group">
+                        <p>{props.nMem} Members</p>
+                    </div>
+                </div>
+                <div className="bottom-searched-group">
+                    <div class = "joinGroupDescription">{props.gDescription}</div>
+                </div>
             </div>
         </div>
 
-        {/* If group is clicked */}
-        {isClicked && (<div className="join-password">
-            Enter password for {props.gName}
-        </div>
-        )}
-        {isClicked && (<div className="password-search"><input type="text" className="password-searchbar" placeholder="Enter password"/></div>)}
-        {isClicked && (<div className="enter-password"><button className="join-group-button">Join Group</button></div>)}
-        </div>
+        {isClicked && (<div className = "PasswordContainer">
+            {isClicked && (<div className="password-search"><input type="text" className="password-searchbar" placeholder="Enter password"/></div>)}
+            {isClicked && (<div className="enter-password"><button className="join-group-button">Join Group</button></div>)}
+        </div>)}
+    </div>
     );
 };
 
