6269615938fb84a157d6c1cd345d1564175a4d8e
racheymonkey <131830335+racheymonkey@users.noreply.github.com>
2023-12-05 17:49:04 -0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,13 @@ import Signup from './Signup/Signup';
 import Update from './Update/Update';
 import HomeScreen from './HomeScreen/HomeScreen';
 import CreateGroup from './CreateGroup/CreateGroup'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import EditProfile from './EditProfile/EditProfile';
+import ViewGroup from './ViewGroup/ViewGroup';
+import JoinGroup from './JoinGroup/JoinGroup';
+import MessageBoard from './MessageBoard/MessageBoard';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 const App = () => {
   return (
@@ -19,8 +24,12 @@ const App = () => {
         <Route path="/Components/Signup" element={<Signup />} />
         <Route path="/HomeScreen" element={<HomeScreen />} />
         <Route path="/CreateGroupRoute" element={<CreateGroup />} />
-        <Route path="/update" element={<Update />} />
         <Route path = "/EditProfile" element = {<EditProfile/>} />
+        <Route path="/ViewGroup" element={<ViewGroup />} />
+        <Route path="/ViewGroup/:groupId" element={<ViewGroup />} />
+        <Route path="/JoinGroup" element={<JoinGroup />} />
+        <Route path="/message" element={<MessageBoard />} />
+        <Route path="/update" element={<Update />} />
       </Routes>
     </Router>
   );
