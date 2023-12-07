5c3d6ebe8fc6aa7d5483059319544e5ec852a3db
ryanvu2 <ryanvuemail@gmail.com>
2023-12-06 20:06:01 -0800

    Addition: EditProfile Backend Implementation

--- client/src/EditProfile/EditProfile.js
+++ client/src/EditProfile/EditProfile.js
@@ -1,81 +1,145 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import { useNavigate} from "react-router-dom"
-import GroupCard from "../Components/GroupCard";
-import ProfileCard from "../Components/ProfileCard";
-import HeadCard from "../HeadCard/HeadCard"
-import React from "react";
-import './EditProfile.css';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import HeadCard from "../HeadCard/HeadCard";
+import "./EditProfile.css";
+import { setCurrentUserId, getCurrentUserId } from '../currentUser'
 
-function EditProfile(){
-    // State to hold the current image URL
-const [imageSrc, setImageSrc] = useState("https://www.booksie.com/files/profiles/22/mr-anonymous.png");
-const openFileSelector = () => {
+function EditProfile() {
+  const navigate = useNavigate();
+
+  const [imageSrc, setImageSrc] = useState(
+    "https://www.booksie.com/files/profiles/22/mr-anonymous.png"
+  );
+  const [formData, setFormData] = useState({
+    name: '',
+    graduationYear: '',
+    major: '',
+    jobPosition: '',
+    company: '',
+  });
+
+  const openFileSelector = () => {
     document.getElementById("fileInput").click();
   };
 
-    const navigate = useNavigate();
-    function HomeRoute(){
-        navigate('/HomeScreen');
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const fileUrl = URL.createObjectURL(file);
+      setImageSrc(fileUrl);
     }
+  };
 
-    const handleFileChange = (event) => {
-        const file = event.target.files[0];
-        if (file) {
-          // Create a URL for the selected file
-          const fileUrl = URL.createObjectURL(file);
-          setImageSrc(fileUrl); // Update the state with the new image URL
-        }
-      };
+  const handleInputChange = (e, fieldName) => {
+    const { value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [fieldName]: value,
+    }));
+  };
 
-    return(
-<div>
-    {/* header outside bc margins */}
-    <HeadCard/>
-    <div class = "ep">
-    <div class = "epCard">
-        <h2 class ="epTitle" >Edit Profile</h2>
-        <   div  className="choose-image-section">
-              {/* Hidden File Input */}
-              <input
-                type="file"
-                id="fileInput"
-                style={{ display: "none" }}
-                accept="image/png, image/jpeg"
-                onChange={handleFileChange}
+  const handleSubmit = async () => {
+    const userId = getCurrentUserId(); // Replace with the actual user ID
+    console.log('User ID:', userId);
+    const apiUrl = `http://localhost:4000/api/users/${userId}`;
+    console.log('User ID:', apiUrl);
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        const updatedUser = await response.json();
+        console.log('User updated:', updatedUser);
+        // Handle success, e.g., update state or navigate to another page
+        navigate('/HomeScreen');
+      } else {
+        console.error('Update failed:', response.statusText);
+        // Handle failure
+      }
+    } catch (error) {
+      console.error('Error:', error.message);
+      // Handle error
+    }
+  };
+
+  const HomeRoute = () => {
+    navigate('/HomeScreen');
+  };
+
+  return (
+    <div>
+      {/* header outside bc margins */}
+      <HeadCard />
+      <div className="ep">
+        <div className="epCard">
+          <h2 className="epTitle">Edit Profile</h2>
+          <div className="choose-image-section">
+            <input
+              type="file"
+              id="fileInput"
+              style={{ display: "none" }}
+              accept="image/png, image/jpeg"
+              onChange={handleFileChange}
+            />
+            <div className="profile-picture-container">
+              <img
+                loading="lazy"
+                src={imageSrc}
+                alt="Profile"
+                className="profile-picture"
               />
-              <div className="profile-picture-container">
-                <img
-                  loading="lazy"
-                  src={imageSrc}
-                  className="profile-picture"
-                />
-              </div>
-              <button id = "epChooseImage" className="new-image-button"
-                onClick={openFileSelector}
-                >
-                Choose Image
-              </button>
-              </div>
-                <p>First and Last Name</p>
-                <input></input>
-                <p>Graduation Year</p>
-                <input></input>
-                <p>Major</p>
-                <input></input>
-                <p>Current Job Position</p>
-                <input></input>
-                <p>Current Company</p>
-                <input></input>
-            <div class = "submitBox" onClick={HomeRoute}>
-                 <button class = "submitButton">Submit</button>
             </div>
+            <button
+              id="epChooseImage"
+              className="new-image-button"
+              onClick={openFileSelector}
+            >
+              Choose Image
+            </button>
+          </div>
+          <p>First and Last Name</p>
+          <input
+            type="text"
+            value={formData.name}
+            onChange={(e) => handleInputChange(e, 'name')}
+          />
+          <p>Graduation Year</p>
+          <input
+            type="text"
+            value={formData.graduationYear}
+            onChange={(e) => handleInputChange(e, 'graduationYear')}
+          />
+          <p>Major</p>
+          <input
+            type="text"
+            value={formData.major}
+            onChange={(e) => handleInputChange(e, 'major')}
+          />
+          <p>Current Job Position</p>
+          <input
+            type="text"
+            value={formData.jobPosition}
+            onChange={(e) => handleInputChange(e, 'jobPosition')}
+          />
+          <p>Current Company</p>
+          <input
+            type="text"
+            value={formData.company}
+            onChange={(e) => handleInputChange(e, 'company')}
+          />
+          <div className="submitBox" onClick={handleSubmit}>
+            <button className="submitButton">Submit</button>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-</div>
-    )
+  );
 }
 
-
-export default EditProfile
+export default EditProfile;
