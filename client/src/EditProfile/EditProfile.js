99b1fa461079d9a394f80c06e519931b70c8d6b1
ryanvu2 <ryanvuemail@gmail.com>
2023-12-06 18:17:49 -0800

    Tweak EditProfile Finalized CSS

--- client/src/EditProfile/EditProfile.js
+++ client/src/EditProfile/EditProfile.js
@@ -6,41 +6,62 @@ import ProfileCard from "../Components/ProfileCard";
 import HeadCard from "../HeadCard/HeadCard"
 import React from "react";
 import './EditProfile.css';
+import { Link } from 'react-router-dom';
 
-function HomeScreen(){
- 
+function EditProfile(){
+    // State to hold the current image URL
+const [imageSrc, setImageSrc] = useState("https://www.booksie.com/files/profiles/22/mr-anonymous.png");
+const openFileSelector = () => {
+    document.getElementById("fileInput").click();
+  };
+
+    const navigate = useNavigate();
+    function HomeRoute(){
+        navigate('/HomeScreen');
+    }
+
     return(
 <div>
     {/* header outside bc margins */}
     <HeadCard/>
     <div class = "ep">
     <div class = "epCard">
-        <h2>Edit Profile</h2>
-        <div class = "epTop">
-            <div class = "epLeft">
+        <h2 class ="epTitle" >Edit Profile</h2>
+        <   div  className="choose-image-section">
+              {/* Hidden File Input */}
+              <input
+                type="file"
+                id="fileInput"
+                style={{ display: "none" }}
+                accept="image/png, image/jpeg"
+                // onChange={handleFileChange}
+              />
+              <div className="profile-picture-container">
+                <img
+                  loading="lazy"
+                  src={imageSrc}
+                  className="profile-picture"
+                />
+              </div>
+              <button id = "epChooseImage" className="new-image-button"
+                onClick={openFileSelector}
+                >
+                Choose Image
+              </button>
+              </div>
                 <p>First and Last Name</p>
                 <input></input>
                 <p>Graduation Year</p>
                 <input></input>
-                <p>Current Occupation</p>
+                <p>Major</p>
+                <input></input>
+                <p>Current Job Position</p>
                 <input></input>
                 <p>Current Company</p>
                 <input></input>
+            <div class = "submitBox" onClick={HomeRoute}>
+                 <button class = "submitButton">Submit</button>
             </div>
-            <div class = "epRight">
-                <p>Major</p>
-                <input></input>
-            </div>
-        </div>
-        <div class = "epBottom">
-            <div>
-                <p>Additional Details/About Me</p>
-                <input></input>
-            </div>
-            <div class = "submitBox">
-                <button class = "submitButton">Submit</button>
-            </div>
-        </div>
     </div>
     </div>
 </div>
@@ -48,4 +69,4 @@ function HomeScreen(){
 }
 
 
-export default HomeScreen
+export default EditProfile
